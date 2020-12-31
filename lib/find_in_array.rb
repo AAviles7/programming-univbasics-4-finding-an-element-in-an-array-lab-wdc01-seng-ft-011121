629333a688b00cf68f4cae19cf3ebbb4a207df08
Abraham Aviles <aaaviles1995@gmail.com>
2020-12-31 06:03:31 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,5 +1,7 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  
+  
   # return array.index(value_to_find) => I thought of this before scrolling down
   #                                      to the conclusion and was confused as to
   #                                      why we need to use a loop
